d6abd04abacfa01f2ac5d6107cd05e02b0edee5e
fbrahman <fbrahman@gmail.com>
2017-05-28 13:28:36 -0700

    added role assignment function based on connection order and added shuffled deck pull down

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,6 @@
-let players = {
-    pName: "",
-    currScore: 0,
-    currHand: [],
-    currSelection: "",
+let player = {
+    role: "",
+    key: ""
 }
 
 let pTestArray = [{
@@ -39,7 +37,15 @@ let pTestArray = [{
 
 let database = firebase.database();
 
-cardRef = database.ref("/cards");
+//will hold the shuffled deck which will decrease as cards are dealt
+let cardRef = database.ref("/cards");
+
+//will hold users as they connect the site
+let userRef = database.ref("/users");
+let connectedRef = database.ref(".info/connected");
+
+//will hold all card related information
+let gameRef = database.ref("/game")
 
 let cards = {
     oDeck: [],
@@ -80,7 +86,8 @@ let cards = {
 
             // console.log("this is inside the for loop ", i, j)
         }
-        this.sDeck = tArray;
+        // this.sDeck = tArray;
+        cardRef.set(tArray);
         return tArray;
     },
 
@@ -97,50 +104,106 @@ let cards = {
 }
 
 let game = {
-        cardsCountedArray: [],
-        //roles and players stored here.
+    cardsCountedArray: [],
+    //roles and players stored here.
+    startGame: function() {
+        this.startGameAssignRoles();
+        cards.createDeck();
+        cards.shuffleDeck(cards.oDeck);
+    },
+    startGameAssignRoles: function() {
+        userRef.orderByChild("dateAdded").once("value").then(function(snap) {
+            let usersArray = snap.val();
+            console.log(usersArray);
+            let userArrayKeys = Object.keys(usersArray);
+            console.log(userArrayKeys);
 
-        scoring: function() {
-            // counting instances of an object
-            let cardsSelected = pTestArray.map(function(item) {
-                return item.voteSelection
+            userRef.child(userArrayKeys[0]).update({
+                role: "storyTeller"
             });
-            // console.log("these are the cards selected", cardsSelected);
-            let countedCards = cardsSelected.reduce(function(allCards, card) {
-                if (card in allCards) {
-                    allCards[card]++;
-                } else {
-                    allCards[card] = 1;
-                }
-                return allCards;
-            }, {});
-            // console.log("this is CountedName", countedCards);
-            this.cardsCountedArray = countedCards;
 
-            let posStoryTeller = pTestArray.map(function(e) {
-                return e.role }).indexOf("storyTeller");
+            for (i = 1; i < userArrayKeys.length; i++) {
+                userRef.child(userArrayKeys[i]).update({
+                    role: "player"
+                })
+            };
+        })
+    },
 
-            console.log("this is the position of the story teller", posStoryTeller);
+    scoring: function() {
+        // counting instances of an object
+        let cardsSelected = pTestArray.map(function(item) {
+            return item.voteSelection
+        });
+        // console.log("these are the cards selected", cardsSelected);
+        let countedCards = cardsSelected.reduce(function(allCards, card) {
+            if (card in allCards) {
+                allCards[card]++;
+            } else {
+                allCards[card] = 1;
+            }
+            return allCards;
+        }, {});
+        // console.log("this is CountedName", countedCards);
+        this.cardsCountedArray = countedCards;
 
-        },
+        let posStoryTeller = pTestArray.map(function(e) {
+            return e.role
+        }).indexOf("storyTeller");
 
-        dealingHand: function(deckArray,nCards) {
-        	let 
+        console.log("this is the position of the story teller", posStoryTeller);
 
-        	for (var i = nCards - 1; i >= 0; i--) {
+    },
 
+    dealingHand: function(deckArray, nCards) {
+        let hand = [];
 
-        		
-        	}
-
+        for (let i = 0; i < nCards; i++) {
+            hand.push(deckArray[0]);
+            deckArray.splice(0, 1);
         }
 
+        cardRef.set(deckArray);
 
-            cards.createDeck();
-        var shuffledDeck = cards.shuffleDeck(cards.oDeck);
+        return hand;
+    },
 
-        cardRef.set(shuffledDeck);
+    checkAndDeal: function() {
 
-        cardRef.once("value", function(snap) {
-            cards.sDeck = snap.val();
-        });
+    }
+}
+
+//on user connection add them to the db
+connectedRef.on("value", function(snap) {
+    if (snap.val()) {
+        var key = userRef.push({
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        }).key;
+
+        userRef.child(key).update({
+            key: key
+        })
+
+        player.key = key;
+        userRef.child(key).onDisconnect().remove();
+    }
+});
+
+
+//automatically start game when three players have joined. This will be assigned to enable the start button in the final game.
+userRef.once("value", function(snap) {
+    numPlayers = snap.numChildren();
+
+    console.log("numPlayers:", numPlayers);
+
+    if (numPlayers > 2) {
+        console.log("inside the start listener if statement");
+        game.startGame();
+    }
+
+})
+
+//insure everyone has the latest shuffled deck locally. Updated as cards are dealt out.
+cardRef.on("value", function(snap) {
+    cards.sDeck = snap.val();
+});
